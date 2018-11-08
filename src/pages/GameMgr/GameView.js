5b018846daaada68e2226964c97a6ed4b575f5a4
ThomasFuzhou <29013551@qq.com>
2018-11-08 11:01:59 +0800

    fix

--- src/pages/GameMgr/GameView.js
+++ src/pages/GameMgr/GameView.js
@@ -28,87 +28,32 @@ const ButtonGroup = Button.Group;
 
 const getWindowWidth = () => window.innerWidth || document.documentElement.clientWidth;
 
-const menu = (
-  <Menu>
-    <Menu.Item key="1">选项一</Menu.Item>
-    <Menu.Item key="2">选项二</Menu.Item>
-    <Menu.Item key="3">选项三</Menu.Item>
-  </Menu>
-);
+
 
 const action = (
   <Fragment>
-    <ButtonGroup>
-      <Button>操作</Button>
-      <Button>操作</Button>
-      <Dropdown overlay={menu} placement="bottomRight">
-        <Button>
-          <Icon type="ellipsis" />
-        </Button>
-      </Dropdown>
-    </ButtonGroup>
+    <Button type="primary">主操作一</Button>
+    <Button type="primary">主操作二</Button>
     <Button type="primary">主操作</Button>
   </Fragment>
 );
 
-const extra = (
-  <Row>
-    <Col xs={24} sm={12}>
-      <div className={styles.textSecondary}>状态</div>
-      <div className={styles.heading}>待审批</div>
-    </Col>
-    <Col xs={24} sm={12}>
-      <div className={styles.textSecondary}>订单金额</div>
-      <div className={styles.heading}>¥ 568.08</div>
-    </Col>
-  </Row>
-);
 
-const description = (
-  <DescriptionList className={styles.headerList} size="small" col="2">
-    <Description term="创建人">曲丽丽</Description>
-    <Description term="订购产品">XX 服务</Description>
-    <Description term="创建时间">2017-07-07</Description>
-    <Description term="关联单据">
-      <a href="">12421</a>
-    </Description>
-    <Description term="生效日期">2017-07-07 ~ 2017-08-08</Description>
-    <Description term="备注">请于两个工作日内确认</Description>
-  </DescriptionList>
-);
-
-const tabList = [
-  {
-    key: 'detail',
-    tab: '详情',
-  },
-  {
-    key: 'rule',
-    tab: '规则',
-  },
-];
 
 const desc1 = (
   <div className={classNames(styles.textSecondary, styles.stepDescription)}>
-    <Fragment>
-      曲丽丽
-      <Icon type="dingding-o" style={{ marginLeft: 8 }} />
-    </Fragment>
-    <div>2016-12-12 12:32</div>
+    2016-12-12 12:32
+  </div>
+);
+const desc2 = (
+  <div className={classNames(styles.textSecondary, styles.stepDescription)}>
+  </div>
+);
+const desc3 = (
+  <div className={classNames(styles.textSecondary, styles.stepDescription)}>
   </div>
 );
 
-const desc2 = (
-  <div className={styles.stepDescription}>
-    <Fragment>
-      周毛毛
-      <Icon type="dingding-o" style={{ color: '#00A0E9', marginLeft: 8 }} />
-    </Fragment>
-    <div>
-      <a href="">催一下</a>
-    </div>
-  </div>
-);
 
 const popoverContent = (
   <div style={{ width: 160 }}>
@@ -131,20 +76,6 @@ const customDot = (dot, { status }) =>
     dot
   );
 
-const operationTabList = [
-  {
-    key: 'tab1',
-    tab: '操作日志一',
-  },
-  {
-    key: 'tab2',
-    tab: '操作日志二',
-  },
-  {
-    key: 'tab3',
-    tab: '操作日志三',
-  },
-];
 
 const columns = [
   {
@@ -229,122 +160,79 @@ class GameView extends Component {
     const { stepDirection, operationkey } = this.state;
     const { profile, loading } = this.props;
     const { advancedOperation1, advancedOperation2, advancedOperation3 } = profile;
-    const contentList = {
-      tab1: (
-        <Table
-          pagination={false}
-          loading={loading}
-          dataSource={advancedOperation1}
-          columns={columns}
-        />
-      ),
-      tab2: (
-        <Table
-          pagination={false}
-          loading={loading}
-          dataSource={advancedOperation2}
-          columns={columns}
-        />
-      ),
-      tab3: (
-        <Table
-          pagination={false}
-          loading={loading}
-          dataSource={advancedOperation3}
-          columns={columns}
-        />
-      ),
-    };
+
 
     return (
       <PageHeaderWrapper
-        title="单号：234231029431"
-        logo={
-          <img alt="" src="https://gw.alipayobjects.com/zos/rmsportal/nxkuOJlFJuAUhzlMTCEe.png" />
-        }
+        title="魔兽世界"
         action={action}
-        content={description}
-        extraContent={extra}
-        tabList={tabList}
+        content={null}
+        extraContent={null}
+        tabList={null}
       >
-        <Card title="流程进度" style={{ marginBottom: 24 }} bordered={false}>
+        <Card title="流程状态（审核中）" style={{ marginBottom: 24 }} bordered={false}>
           <Steps direction={stepDirection} progressDot={customDot} current={1}>
-            <Step title="创建项目" description={desc1} />
-            <Step title="部门初审" description={desc2} />
-            <Step title="财务复核" />
-            <Step title="完成" />
+            <Step title="提交发布" description={desc1} />
+            <Step title="审核中" description={desc2} />
+            <Step title="上线" description={desc3}/>
           </Steps>
         </Card>
-        <Card title="用户信息" style={{ marginBottom: 24 }} bordered={false}>
-          <DescriptionList style={{ marginBottom: 24 }}>
-            <Description term="用户姓名">付小小</Description>
-            <Description term="会员卡号">32943898021309809423</Description>
-            <Description term="身份证">3321944288191034921</Description>
-            <Description term="联系方式">18112345678</Description>
-            <Description term="联系地址">
-              曲丽丽 18100000000 浙江省杭州市西湖区黄姑山路工专路交叉路口
-            </Description>
-          </DescriptionList>
-          <DescriptionList style={{ marginBottom: 24 }} title="信息组">
-            <Description term="某某数据">725</Description>
-            <Description term="该数据更新时间">2017-08-08</Description>
-            <Description>&nbsp;</Description>
-            <Description
-              term={
-                <span>
-                  某某数据
-                  <Tooltip title="数据说明">
-                    <Icon
-                      style={{ color: 'rgba(0, 0, 0, 0.43)', marginLeft: 4 }}
-                      type="info-circle-o"
-                    />
-                  </Tooltip>
-                </span>
-              }
-            >
-              725
-            </Description>
-            <Description term="该数据更新时间">2017-08-08</Description>
-          </DescriptionList>
-          <h4 style={{ marginBottom: 16 }}>信息组</h4>
-          <Card type="inner" title="多层级信息组">
-            <DescriptionList size="small" style={{ marginBottom: 16 }} title="组名称">
-              <Description term="负责人">林东东</Description>
-              <Description term="角色码">1234567</Description>
-              <Description term="所属部门">XX公司 - YY部</Description>
-              <Description term="过期时间">2017-08-08</Description>
-              <Description term="描述">
-                这段描述很长很长很长很长很长很长很长很长很长很长很长很长很长很长...
-              </Description>
-            </DescriptionList>
-            <Divider style={{ margin: '16px 0' }} />
-            <DescriptionList size="small" style={{ marginBottom: 16 }} title="组名称" col="1">
-              <Description term="学名">
-                Citrullus lanatus (Thunb.) Matsum. et
-                Nakai一年生蔓生藤本；茎、枝粗壮，具明显的棱。卷须较粗..
-              </Description>
-            </DescriptionList>
-            <Divider style={{ margin: '16px 0' }} />
-            <DescriptionList size="small" title="组名称">
-              <Description term="负责人">付小小</Description>
-              <Description term="角色码">1234568</Description>
-            </DescriptionList>
-          </Card>
+        <Card style={null} bordered={false}>
+          <Row style={{ marginBottom: 16 }}>
+              <Col sm={24} xs={24}><h3><b>游戏信息</b></h3></Col>
+          </Row>
+          <Row style={{ marginBottom: 32 }}>
+            <Col sm={8} xs={12}>
+              游戏类型：休闲益智 角色扮演 射击
+            </Col>
+            <Col sm={8} xs={12}>
+              开发者：张祖钦工作室
+            </Col>
+            <Col sm={8} xs={12}>
+              发布时间：2018-11-4 21:05:26
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: 32 }}>
+              <Col sm={24} xs={24}>联系地址：显示完整简介内容，支持换行。显示完整简介内容，支持换行。显示完整简介内容，支持换行。显示完整简介内容，支持换行。显示完整简介内容，支持换行。显示完整简介内容，支持换行。显示完整简介内容，支持换行。显示完整简介内容，支持换行。</Col>
+          </Row>
+
+          <Divider style={{ margin: '20px 0' }} />
+          <Row style={{ marginBottom: 16 }}>
+              <Col sm={24} xs={24}><h3><b>版本信息</b></h3></Col>
+          </Row>
+          <Row style={{ marginBottom: 32 }}>
+            <Col sm={8} xs={12}>
+              当前版本：V2.0.0
+            </Col>
+            <Col sm={8} xs={12}>
+              更新时间：2018-11-4 21:05:26
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: 32 }}>
+              <Col sm={24} xs={24}>更新内容：显示完整简介内容，支持换行。显示完整简介内容，支持换行。显示完整简介内容，支持换行。</Col>
+          </Row>
+
+          <Divider style={{ margin: '20px 0' }} />
+          <Row style={{ marginBottom: 16 }}>
+              <Col sm={24} xs={24}><h3><b>素材信息</b></h3></Col>
+          </Row>
+          <Row style={{ marginBottom: 32 }}>
+            <Col sm={24} xs={24}>
+              游戏图标：
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: 32 }}>
+            <Col sm={24} xs={24}>
+              封面图片：
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: 32 }}>
+              <Col sm={24} xs={24}>
+                游戏截图：
+              </Col>
+          </Row>
         </Card>
-        <Card title="用户近半年来电记录" style={{ marginBottom: 24 }} bordered={false}>
-          <div className={styles.noData}>
-            <Icon type="frown-o" />
-            暂无数据
-          </div>
-        </Card>
-        <Card
-          className={styles.tabsCard}
-          bordered={false}
-          tabList={operationTabList}
-          onTabChange={this.onOperationTabChange}
-        >
-          {contentList[operationkey]}
-        </Card>
+
       </PageHeaderWrapper>
     );
   }
