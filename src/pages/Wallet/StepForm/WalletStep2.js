4a51d4fc0130ebd74c2d0cee3ae4ce8064e8b2b7
ThomasFuzhou <29013551@qq.com>
2018-11-12 16:03:53 +0800

    助记词

--- src/pages/Wallet/StepForm/WalletStep2.js
+++ src/pages/Wallet/StepForm/WalletStep2.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'dva';
 import { Form, Input, Button, Alert, Divider } from 'antd';
 import router from 'umi/router';
@@ -41,6 +41,7 @@ class WalletStep2 extends React.PureComponent {
       });
     };
     return (
+      <Fragment>
       <Form layout="horizontal" className={styles.stepForm}>
         <Alert
           closable
@@ -92,6 +93,22 @@ class WalletStep2 extends React.PureComponent {
           </Button>
         </Form.Item>
       </Form>
+        <Divider style={{ margin: '40px 0' }} />
+        <div className={styles.desc}>
+          <h4>为什么要备份钱包？</h4>
+          <p>
+            游戏金钱包基于区块链技术，是完全属于您个人的去中心化钱包，钱包备份是您恢复钱包的唯一途径，因此强烈建议您立即备份钱包，并妥善保管好您的备份，切勿丢失或告知他人。
+          </p>
+          <h4>如何备份钱包？</h4>
+          <p>
+            点击备份钱包，屏幕上会出现12个汉字作为助记词，您务必将12个汉字助记词用纸笔抄写下来，这是您将来恢复钱包的重要凭证。请勿将助记词告知他人或使用不安全的方式保存。
+          </p>
+          <h4>如何恢复钱包？</h4>
+          <p>
+            点击备份钱包，屏幕上会出现12个汉字作为助记词，您务必将12个汉字助记词用纸笔抄写下来，这是您将来恢复钱包的重要凭证。请勿将助记词告知他人或使用不安全的方式保存。
+          </p>
+        </div>
+      </Fragment>
     );
   }
 }
