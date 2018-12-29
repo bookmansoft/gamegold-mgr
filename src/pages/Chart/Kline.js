466de92a657107422d945a914aa9d2da3a6f950d
ThomasFuzhou <29013551@qq.com>
2018-12-29 10:58:36 +0800

    fix

--- src/pages/Chart/Kline.js
+++ src/pages/Chart/Kline.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Link from 'umi/link';
 import { formatMessage } from "umi/locale";
-import Exception from '@/components/Exception';
-
+import { Card } from 'antd';
+import router from 'umi/router';
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 export default () => (
-  <iframe src="http://localhost:9101/client/echart/kline/k1.html" frameborder="0" width="100%" height="600px" scrolling = "no"/>
+  <PageHeaderWrapper>
+    <Card bordered={false}>
+      <iframe src="http://localhost:9101/client/echart/kline/k1.html" frameborder="0" width="100%" height="600px" scrolling="no" />
+    </Card>
+  </PageHeaderWrapper>
 );
+
+
+
