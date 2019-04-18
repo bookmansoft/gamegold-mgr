166dd3f50d8602985ac8135aa26f17d9d3a49fd3
ThomasFuzhou <29013551@qq.com>
2019-04-18 17:27:16 +0800

    fix

---
+++ src/pages/Market/models/maketview.js
@@ -0,0 +1,30 @@
+import { getGameView,getGameFromUrl } from '@/services/gamegoldapi';
+
+export default {
+  namespace: 'marketview',
+
+  state: {
+    data: {
+    }
+  },
+
+  effects: {
+    *fetch({ payload }, { call, put }) {
+      console.log("gameview model："+payload.id);
+      const response = yield call(getGameView, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+    },
+  },
+
+  reducers: {
+    save(state, action) {
+      return {
+        ...state,
+        data: action.payload,
+      };
+    },
+  },
+};
