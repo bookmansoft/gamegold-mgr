f8aabd27e545e74583eaaf42703b41cc08af822f
ThomasFuzhou <29013551@qq.com>
2019-04-18 15:30:50 +0800

    fix

---
+++ src/pages/Funding/FundingApply.js
@@ -0,0 +1,293 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'dva';
+import { formatMessage, FormattedMessage } from 'umi/locale';
+import router from 'umi/router';
+import moment from 'moment';
+import {
+  Form,
+  Input,
+  DatePicker,
+  Select,
+  Button,
+  Card,
+  Row,
+  Col,
+  Divider,
+  InputNumber,
+  Radio,
+  Icon,
+  Tooltip,
+} from 'antd';
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+import styles from './style.less';
+
+const FormItem = Form.Item;
+const { Option } = Select;
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+
+@connect(({ fundingapply, loading }) => ({
+  fundingapply,
+  loading: loading.models.fundingapply,
+  submitting: loading.effects['fundingapply/add'],
+}))
+@Form.create()
+class FundingApply extends PureComponent {
+  componentDidMount() {
+    const { dispatch } = this.props;
+
+  }
+  //创建
+  handleCreate = (theData) => {
+    const { dispatch, form } = this.props;
+    console.log(theData);
+    dispatch({
+      type: 'fundingapply/add',
+      payload: theData,
+    }).then((ret) => {
+      console.log(ret);
+      if (ret.code === 0) {
+        router.push('/fundingapply/fundingapplysuccess');
+      } else {
+        router.push('/fundingapply/fundingapplyerror');
+      };
+    }
+    );
+  };
+
+  renderImg = (text) => {
+    if (text && text.length) {
+      const imgs = text.map((item, index) =>
+        <img width={120} src={item} key={index} />
+      )
+      return imgs;
+    }
+  }
+
+  //获取URL内容的操作
+  handleSubmit = e => {
+    const { dispatch, form } = this.props;
+    e.preventDefault();
+    form.validateFieldsAndScroll((err, values) => {
+      console.log(values);
+      if (!err) {
+        dispatch({
+          type: 'fundingapply/fetch',
+          payload: values,
+        });
+      };
+    });
+  }
+  render() {
+    const { submitting } = this.props;
+    const {
+      fundingapply: { data },
+      form: { getFieldDecorator, getFieldValue },
+    } = this.props;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 3 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 15 },
+      },
+    };
+
+    const formItemLayout2 = {
+      labelCol: {
+        xs: { span: 12 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 12 },
+        sm: { span: 16 },
+      },
+    };
+
+    const submitFormLayout = {
+      wrapperCol: {
+        xs: { span: 24, offset: 0 },
+        sm: { span: 10, offset: 7 },
+      },
+    };
+
+    return (
+      <PageHeaderWrapper
+        title="众筹申请"
+        content=""
+      >
+        <Form onSubmit={this.handleSubmit} hideRequiredMark={false} style={{ marginTop: 8 }}>
+          <Card bordered={false}>
+            <Row style={{ marginBottom: 32 }}>
+              <br />
+              <h2><b>众筹申请</b></h2>
+              <br />
+            </Row>
+            <Row>
+              <FormItem {...formItemLayout} label="游戏URL链接">
+                {getFieldDecorator('cp_url', {
+                  rules: [
+                    {
+                      required: true,
+                      message: "请输入游戏URL链接",
+                    },
+                  ],
+                })(<Input placeholder="请输入" />)}
+              </FormItem>
+              <FormItem {...formItemLayout} label="结算钱包地址">
+                {getFieldDecorator('wallet_addr', {
+                  rules: [
+                    {
+                      required: true,
+                      message: "请输入结算钱包地址",
+                    },
+                  ],
+                })(<Input placeholder="请输入" />)}
+              </FormItem>
+            </Row>
+            <Row>
+              <Col span={8}>
+                <FormItem {...formItemLayout2} label="使用邀请奖励">
+                  {getFieldDecorator('use_invite_share', {
+                    initialValue: '1',
+                    rules: [
+                      {
+                        required: true,
+                      },
+                    ],
+                  })(
+                  <Select placeholder="请选择" style={{ width: '60px',display:'block', }}>
+                    <Option value="1">是</Option>
+                    <Option value="0">否</Option>
+                  </Select>
+                  )}
+                </FormItem>
+              </Col>
+              <Col span={8}>
+                <FormItem {...formItemLayout2} label="邀请奖励比例">
+                  {getFieldDecorator('invite_share', {
+                    initialValue: '15',
+                  })
+                    (
+                    <Select
+                      placeholder='邀请奖励比例'
+                      style={{
+                        width: '100px',
+                        // display: getFieldValue('use_invite_share') === '1' ? 'block' : 'none',
+                      }}
+                      disabled={getFieldValue('use_invite_share') === '0'}
+                    >
+                      <Option value="1">1%</Option>
+                      <Option value="2">2%</Option>
+                      <Option value="3">3%</Option>
+                      <Option value="4">4%</Option>
+                      <Option value="5">5%</Option>
+                      <Option value="6">6%</Option>
+                      <Option value="7">7%</Option>
+                      <Option value="8">8%</Option>
+                      <Option value="9">9%</Option>
+                      <Option value="10">10%</Option>
+                      <Option value="11">11%</Option>
+                      <Option value="12">12%</Option>
+                      <Option value="13">13%</Option>
+                      <Option value="14">14%</Option>
+                      <Option value="15">15%</Option>
+                      <Option value="16">16%</Option>
+                      <Option value="17">17%</Option>
+                      <Option value="18">18%</Option>
+                      <Option value="19">19%</Option>
+                      <Option value="20">20%</Option>
+                      <Option value="21">21%</Option>
+                      <Option value="22">22%</Option>
+                      <Option value="23">23%</Option>
+                      <Option value="24">24%</Option>
+                      <Option value="25">25%</Option>
+                      <Option value="26">26%</Option>
+                      <Option value="27">27%</Option>
+                      <Option value="28">28%</Option>
+                      <Option value="29">29%</Option>
+                      <Option value="30">30%</Option>
+                    </Select>
+                    )}
+                </FormItem>
+              </Col>
+              <Col md={4}>
+                <Button type="primary" htmlType="submit" loading={submitting}>
+                  验证
+              </Button>
+              </Col>
+            </Row>
+            <br />
+            <h2><b>基本信息预览</b></h2>
+            <br />
+            <Row style={{ marginBottom: 32 }}>
+              <Col sm={8} xs={12}>游戏名称：{data.cp_text}</Col>
+              <Col sm={8} xs={12}>游戏简称：{data.cp_name}</Col>
+            </Row>
+            <Row style={{ marginBottom: 32 }}>
+              <Col sm={8} xs={12}>
+                游戏类型：{data.cp_type}
+              </Col>
+              <Col sm={8} xs={12}>
+                开发者：{data.develop_name}
+              </Col>
+              <Col sm={8} xs={12}>
+                发布时间：{!!data.publish_time && moment(data.publish_time * 1000).format('YYYY-MM-DD HH:mm:ss')}
+              </Col>
+            </Row>
+            <Row style={{ marginBottom: 32 }}>
+              <Col sm={24} xs={24}>URL地址：{data.cp_url}</Col>
+            </Row>
+
+            <Divider style={{ margin: '20px 0' }} />
+            <Row style={{ marginBottom: 16 }}>
+              <Col sm={24} xs={24}><h3><b>版本信息</b></h3></Col>
+            </Row>
+            <Row style={{ marginBottom: 32 }}>
+              <Col sm={8} xs={12}>
+                当前版本：{data.cp_version}
+              </Col>
+              <Col sm={8} xs={12}>
+                更新时间：{data.publish_time}
+              </Col>
+            </Row>
+            <Row style={{ marginBottom: 32 }}>
+              <Col sm={24} xs={24}>更新内容：{data.cp_desc}</Col>
+            </Row>
+
+            <Divider style={{ margin: '20px 0' }} />
+            <Row style={{ marginBottom: 16 }}>
+              <Col sm={24} xs={24}><h3><b>素材信息</b></h3></Col>
+            </Row>
+            <Row style={{ marginBottom: 32 }}>
+              <Col sm={24} xs={24}>
+                游戏图标：<img width={120} src={data.icon_url} />
+              </Col>
+            </Row>
+            <Row style={{ marginBottom: 32 }}>
+              <Col sm={24} xs={24}>
+                封面图片：<img width={120} src={data.face_url} />
+              </Col>
+            </Row>
+            <Row style={{ marginBottom: 32 }}>
+              <Col sm={24} xs={24}>
+                游戏截图：{this.renderImg(data.pic_urls)}
+              </Col>
+            </Row>
+            <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
+              <Button type="primary" onClick={() => this.handleCreate(this.props.fundingapply.data)}>
+                提交
+              </Button>
+            </FormItem>
+          </Card>
+        </Form>
+
+      </PageHeaderWrapper>
+    );
+  }
+}
+
+export default FundingApply;
---
+++ src/pages/Funding/GameList.js
@@ -0,0 +1,261 @@
+import React, { PureComponent, Fragment } from 'react';
+import { connect } from 'dva';
+import moment from 'moment';
+import {
+  Row,
+  Col,
+  Card,
+  Form,
+  Input,
+  Select,
+  Button,
+  Steps,
+  Radio,
+} from 'antd';
+import SimpleTable from '@/components/SimpleTable';
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+
+import styles from './GameList.less';
+
+const FormItem = Form.Item;
+const { Option } = Select;
+const getValue = obj =>
+  Object.keys(obj)
+    .map(key => obj[key])
+    .join(',');
+
+/* eslint react/no-multi-comp:0 */
+@connect(({ gamelist, loading }) => ({
+  gamelist,
+  loading: loading.models.gamelist,
+}))
+@Form.create()
+class GameList extends PureComponent {
+  state = {
+    modalVisible: false,
+    updateModalVisible: false,
+    expandForm: false,
+    selectedRows: [],
+    formValues: {},
+    stepFormValues: {},
+  };
+
+  columns = [
+    {
+      title: '流水号',
+      dataIndex: 'id',
+    },
+    {
+      title: '游戏ID',
+      dataIndex: 'cp_id',
+    },
+    {
+      title: '游戏全名',
+      dataIndex: 'cp_text',
+    },
+    {
+      title: '游戏类型',
+      dataIndex: 'cp_type',
+    },
+    {
+      title: '游戏状态',
+      dataIndex: 'cp_state',
+      render: val => <span>{(val == '0') ? '未上线' : '正常运营'}</span>
+    },
+    {
+      title: '添加时间',
+      dataIndex: 'publish_time',
+      render: val => <span>{moment(val * 1000).format('YYYY-MM-DD HH:mm:ss')}</span>,
+    },
+    {
+      title: '操作',
+      render: (text, record) => (
+        <Fragment>
+          <a onClick={() => this.handleView(true, record)}>详情</a>&nbsp;
+        </Fragment>
+      ),
+    },
+  ];
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'gamelist/fetch',
+    });
+    dispatch({
+      type: 'gamelist/fetchCpType'
+    });
+  }
+
+  handleStandardTableChange = (pagination, filtersArg, sorter) => {
+    const { dispatch } = this.props;
+    const { formValues } = this.state;
+
+    const filters = Object.keys(filtersArg).reduce((obj, key) => {
+      const newObj = { ...obj };
+      newObj[key] = getValue(filtersArg[key]);
+      return newObj;
+    }, {});
+
+    const params = {
+      currentPage: pagination.current,
+      pageSize: pagination.pageSize,
+      ...formValues,
+      ...filters,
+    };
+    if (sorter.field) {
+      params.sorter = `${sorter.field}_${sorter.order}`;
+    }
+
+    dispatch({
+      type: 'gamelist/fetch',
+      payload: params,
+    });
+  };
+
+  handleFormReset = () => {
+    const { form, dispatch } = this.props;
+    form.resetFields();
+    this.setState({
+      formValues: {},
+    });
+    dispatch({
+      type: 'gamelist/fetch',
+      payload: {},
+    });
+  };
+
+  handleSearch = e => {
+    e.preventDefault();
+
+    const { dispatch, form } = this.props;
+
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+
+      const values = {
+        ...fieldsValue,
+        updatedAt: fieldsValue.updatedAt && fieldsValue.updatedAt.valueOf(),
+      };
+
+      this.setState({
+        formValues: values,
+      });
+
+      dispatch({
+        type: 'gamelist/fetch',
+        payload: values,
+      });
+    });
+  };
+
+  //查看页面
+  handleView = (flag, record) => {
+    console.log(record);
+    // this.props.history.push("./gameview?id="+record.cp_id);
+    this.props.history.push("./gameview?id=" + record.id);
+  };
+
+  //赠送道具
+  handleDeal = (flag, record) => {
+    this.setState({
+      updateModalVisible: !!flag,
+      stepFormValues: record || {},
+    });
+  };
+
+  //显示下拉框
+  renderOptions = () => {
+    // console.log(this.props.gamelist.data);
+    if (this.props.gamelist.cp_type_list != null) {
+      return this.props.gamelist.cp_type_list.map(element =>
+        <Option key={element.id} value={element.cp_type_id}> {element.cp_type_id}</Option>);
+    }
+    else {
+      return "";
+    }
+
+  };
+
+  renderForm() {
+    const {
+      form: { getFieldDecorator },
+    } = this.props;
+    return (
+      <Form onSubmit={this.handleSearch} layout="inline">
+        <Row gutter={{ md: 16, lg: 24, xl: 48 }}>
+          <Col md={6} sm={9}>
+            <FormItem label="游戏全名：">
+              {getFieldDecorator('cp_text')(<Input placeholder="请输入" />)}
+            </FormItem>
+          </Col>
+          <Col md={6} sm={9}>
+            <FormItem label="游戏ID：">
+              {getFieldDecorator('cp_id')(<Input placeholder="请输入" />)}
+            </FormItem>
+          </Col>
+          <Col md={6} sm={9}>
+            <FormItem label="游戏类型：">
+              {getFieldDecorator('cp_type')(
+                <Select placeholder="请选择" style={{ width: '100%' }}>
+                  <Option value="">全部</Option>
+                  {this.renderOptions()}
+                </Select>
+              )}
+            </FormItem>
+          </Col>
+          {/* <Col md={6} sm={9}>
+            <FormItem label="状态：">
+              {getFieldDecorator('cp_state')(
+                <Select placeholder="请选择" style={{ width: '100%' }}>
+                  <Option value="1">待审核</Option>
+                  <Option value="2">已上架</Option>
+                  <Option value="3">审核不通过</Option>
+                </Select>
+              )}
+            </FormItem>
+          </Col> */}
+          <Col md={6} sm={9}>
+            <span className={styles.submitButtons}>
+              <Button type="primary" htmlType="submit">
+                搜索
+              </Button>
+              <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
+                重置
+              </Button>
+            </span>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+
+  render() {
+    const {
+      gamelist: { data },
+      loading,
+    } = this.props;
+    const { selectedRows, modalVisible, updateModalVisible, stepFormValues } = this.state;
+
+    return (
+      <PageHeaderWrapper title="游戏列表">
+        <Card bordered={false}>
+          <div className={styles.tableList}>
+            <div className={styles.tableListForm}>{this.renderForm()}</div>
+            <div className={styles.tableListOperator} />
+            <SimpleTable
+              selectedRows={selectedRows}
+              loading={loading}
+              data={data}
+              columns={this.columns}
+              onSelectRow={null}
+              onChange={this.handleStandardTableChange}
+            />
+          </div>
+        </Card>
+      </PageHeaderWrapper>
+    );
+  }
+}
+
+export default GameList;
